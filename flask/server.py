0ecbb90d4ca57b0753d641db762b87c390f6bb42
hiimchinh <chinhhm@proton.me>
2024-09-18 23:23:11 +0700

    add route list

--- flask/server.py
+++ flask/server.py
@@ -17,5 +17,12 @@ def todo():
     return "Hello from the MongoDB client!\n"
 
 
+@app.route('/users')
+def list_users():
+    db = client.crud
+    collection = db.users
+    users = collection.find()
+    return f"Users: {', '.join([user['name'] for user in users])}\n"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
