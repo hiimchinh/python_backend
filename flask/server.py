f0188204dc699a72cb7310d2a4957bd3c98d6706
hiimchinh <chinhhm@proton.me>
2024-09-22 00:08:09 +0700

    add endpoints to insert user

--- flask/server.py
+++ flask/server.py
@@ -3,10 +3,12 @@ import os
 
 from flask import Flask
 from pymongo import MongoClient
+from flask import request
 
 app = Flask(__name__)
+mongo_url = 'mongodb+srv://chinh:575xQvf7lGNFuGFl@cluster-01.kz6vr.mongodb.net/'
 
-client = MongoClient("mongo:27017")
+client = MongoClient(mongo_url)
 
 @app.route('/')
 def todo():
@@ -23,6 +25,15 @@ def list_users():
     collection = db.users
     users = collection.find()
     return f"Users: {', '.join([user['name'] for user in users])}\n"
+
+@app.route('/users', methods=['POST'])
+def create_user():
+
+    db = client.crud
+    collection = db.users
+    user = request.json
+    result = collection.insert_one(user)
+    return f"User created with id: {result.inserted_id}\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
