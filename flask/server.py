ea9f532715ec3994764e173a258b81591ee25d7a
hiimchinh <chinhhm@proton.me>
2024-09-22 09:07:37 +0700

    add route to update data

--- flask/server.py
+++ flask/server.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from pymongo import MongoClient
 from flask import request
+from bson import ObjectId
 
 app = Flask(__name__)
 mongo_url = 'mongodb+srv://chinh:575xQvf7lGNFuGFl@cluster-01.kz6vr.mongodb.net/'
 
 client = MongoClient(mongo_url)
+db = client.crud
 
 @app.route('/')
 def todo():
@@ -18,22 +20,50 @@ def todo():
         return "Server not available"
     return "Hello from the MongoDB client!\n"
 
+def sanitize_request_data(request):
+    allowed_fields = ['address', 'avatar', 'email', 'gender', 'name', 'phone', 'rule', 'status']
+    return {field: request[field] for field in allowed_fields if field in request}
 
-@app.route('/users')
-def list_users():
-    db = client.crud
+@app.route('/users/<id>')
+def get_user(id):
     collection = db.users
-    users = collection.find()
-    return f"Users: {', '.join([user['name'] for user in users])}\n"
+    user = collection.find_one({'_id': ObjectId(id)})
+    if user is None:
+        return "User not found", 404
+    user['_id'] = str(user['_id'])
+    return jsonify(user)
+
+@app.route('/users', methods=['GET'])
+def list_users():
+    collection = db.users
+    users = list(collection.find({}))
+    for user in users:
+        user['_id'] = str(user['_id'])
+    return jsonify(users)
 
 @app.route('/users', methods=['POST'])
 def create_user():
-
-    db = client.crud
     collection = db.users
-    user = request.json
+    request_data = request.json
+    user = sanitize_request_data(request_data)
     result = collection.insert_one(user)
-    return f"User created with id: {result.inserted_id}\n"
+    new_user = collection.find_one({'_id': result.inserted_id})
+    new_user['_id'] = str(new_user['_id'])
+    return jsonify(new_user)
+
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    collection = db.users
+    request_data = request.json
+    user = sanitize_request_data(request_data)
+    result = collection.update_one({'_id': ObjectId(id)}, {'$set': user})
+    return jsonify(result)
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    collection = db.users
+    result = collection.delete_one({'_id': ObjectId(id)})
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
