4194b820139db9e7b7e94c5c8b6a74137801a226
hiimchinh <chinhhm@proton.me>
2024-09-22 10:00:11 +0700

    move user to new blueprint

--- flask/server.py
+++ flask/server.py
@@ -6,9 +6,10 @@ from pymongo import MongoClient
 from flask import request
 from bson import ObjectId
 from products import products_bp
-
+from users import users_bp
 app = Flask(__name__)
 app.register_blueprint(products_bp)
+app.register_blueprint(users_bp)
 mongo_url = 'mongodb+srv://chinh:575xQvf7lGNFuGFl@cluster-01.kz6vr.mongodb.net/'
 
 client = MongoClient(mongo_url)
@@ -21,54 +22,6 @@ def todo():
     except:
         return "Server not available"
     return "Hello from the MongoDB client!\n"
-
-def sanitize_request_data(request):
-    allowed_fields = ['address', 'avatar', 'email', 'gender', 'name', 'phone', 'rule', 'status']
-    return {field: request[field] for field in allowed_fields if field in request}
-
-@app.route('/users/<id>')
-def get_user(id):
-    collection = db.users
-    user = collection.find_one({'_id': ObjectId(id)})
-    if user is None:
-        return "User not found", 404
-    user['_id'] = str(user['_id'])
-    return jsonify(user)
-
-@app.route('/users', methods=['GET'])
-def list_users():
-    collection = db.users
-    users = list(collection.find({}))
-    for user in users:
-        user['_id'] = str(user['_id'])
-    return jsonify(users)
-
-@app.route('/users', methods=['POST'])
-def create_user():
-    collection = db.users
-    request_data = request.json
-    user = sanitize_request_data(request_data)
-    result = collection.insert_one(user)
-    if result.inserted_id is None:
-        return jsonify({"message": "User created failed"}), 500
-    inserted_id = str(result.inserted_id)
-    return jsonify({"_id": inserted_id, "message": "User created successfully"}), 201
-
-@app.route('/users/<id>', methods=['PUT'])
-def update_user(id):
-    collection = db.users
-    request_data = request.json
-    user = sanitize_request_data(request_data)
-    result = collection.update_one({'_id': ObjectId(id)}, {'$set': user})
-    if result.modified_count == 0:
-        return jsonify({"message": "User updated failed"}), 500
-    return jsonify({"message": "User updated successfully"}), 200
-
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    collection = db.users
-    result = collection.delete_one({'_id': ObjectId(id)})
-    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
---
+++ flask/users.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, jsonify, request
+from pymongo import MongoClient
+from bson import ObjectId
+
+users_bp = Blueprint('users', __name__)
+
+mongo_url = 'mongodb+srv://chinh:575xQvf7lGNFuGFl@cluster-01.kz6vr.mongodb.net/'
+client = MongoClient(mongo_url)
+db = client.crud
+
+def sanitize_request_data(request):
+    allowed_fields = ['address', 'avatar', 'email', 'gender', 'name', 'phone', 'rule', 'status']
+    return {field: request[field] for field in allowed_fields if field in request}
+
+@users_bp.route('/users/<id>')
+def get_user(id):
+    collection = db.users
+    user = collection.find_one({'_id': ObjectId(id)})
+    if user is None:
+        return "User not found", 404
+    user['_id'] = str(user['_id'])
+    return jsonify(user)
+
+@users_bp.route('/users', methods=['GET'])
+def list_users():
+    collection = db.users
+    users = list(collection.find({}))
+    for user in users:
+        user['_id'] = str(user['_id'])
+    return jsonify(users)
+
+@users_bp.route('/users', methods=['POST'])
+def create_user():
+    collection = db.users
+    request_data = request.json
+    user = sanitize_request_data(request_data)
+    result = collection.insert_one(user)
+    if result.inserted_id is None:
+        return jsonify({"message": "User created failed"}), 500
+    inserted_id = str(result.inserted_id)
+    return jsonify({"_id": inserted_id, "message": "User created successfully"}), 201
+
+@users_bp.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    collection = db.users
+    request_data = request.json
+    user = sanitize_request_data(request_data)
+    result = collection.update_one({'_id': ObjectId(id)}, {'$set': user})
+    if result.modified_count == 0:
+        return jsonify({"message": "User updated failed"}), 500
+    return jsonify({"message": "User updated successfully"}), 200
+
+@users_bp.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    collection = db.users
+    result = collection.delete_one({'_id': ObjectId(id)})
+    return jsonify(result)
